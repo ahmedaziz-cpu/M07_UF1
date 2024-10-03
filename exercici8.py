b2c45242fea41b51ed0b8af6c15d1a7148f9270c
ahmedaziz-cpu <2024_ahmed.aziz@iticbcn.cat>
2024-10-03 21:12:47 +0200

    Delete exercici8.py

--- exercici8.py
+++
@@ -1,6 +0,0 @@
-paraules = input("Escriu de 1 a 3 paraules: ").split() #  split() divideix la cadena en una llista de paraules
-if len(paraules) < 1 or len(paraules) > 3: # longitud de les paraules maxim 3 paraules minim 1
-    print("ERROR!")
-else:
-    for paraula in paraules:
-        print(f"Paraula: {paraula}, Lletres: {len(paraula)}, Primera lletra: {paraula[0]}, Ultima lletra: {paraula[-1]}")
