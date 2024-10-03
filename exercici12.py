2f9bf7525d3a19b6b3627648179603e7ee2aafba
ahmedaziz-cpu <2024_ahmed.aziz@iticbcn.cat>
2024-10-03 21:11:14 +0200

    Delete exercici12.py

--- exercici12.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("Numero: "))
-
-suma = 0
-
-for x in range(1, numero + 1):
-    suma += x  ##afegim el numero actual
-
-print(f"La suma dels numeros des de 1 fins a {numero} es: {suma}")
